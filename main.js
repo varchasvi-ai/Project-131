a1af2d2803c8642feef123976b5d1f13ec203969
varchasvi-ai <76240712+varchasvi-ai@users.noreply.github.com>
2021-12-04 20:35:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function redirect1()
+{
+    window.location = "image1.html";
+}
+
+function redirect2()
+{
+    window.location = "image2.html";
+}
+
+function redirect3()
+{
+    window.location = "image3.html";
+}
+
+function redirect4()
+{
+    window.location = "image4.html";
+}
+
+function redirect5()
+{
+    window.location = "image5.html";
+}
+
+function back()
+{
+    window.location = "home.html";
+}
